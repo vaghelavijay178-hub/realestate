6450233e51f65d513a8ca49c584dae0ffa422db9
vaghelavijay178-hub <vaghelavijay178@gmail.com>
2026-09-18 12:31:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -185,7 +185,7 @@ function ReelsCarousel({ reels }: { reels: typeof REELS }) {
                   <>
                     <div className="reel-iframe-wrap">
                       <iframe
-                        src={"https://www.youtube.com/embed/wDOQ7L9h-rw?autoplay=1&mute=1&loop=1&playlist=wDOQ7L9h-rw&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1&disablekb=1&fs=0"}
+                        src={"https://www.youtube.com/embed/k7h5ch2lbgg?autoplay=1&mute=1&loop=1&playlist=k7h5ch2lbgg&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1&disablekb=1&fs=0"}
                         allow="autoplay; fullscreen"
                         allowFullScreen
                       />
